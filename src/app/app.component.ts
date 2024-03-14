1c6b5032261cc95650fd94e5131cab7215fd5dca
Fernando FR <ferlicio@gmail.com>
2024-03-14 12:40:56 -0300

    renomeacao do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'BraFurries-Front-End';
+  title = 'BraFurries - Furries do Brasil';
 }
