06dad463e462190cf73fc54f404536fba6b1c43b
Fernando FR <ferlicio@gmail.com>
2024-03-14 12:30:29 -0300

    recriacao do projeto + definicao de paginas e rotas principais

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Brafurries-Front-End' title`, () => {
+  it(`should have as title 'BraFurries-Front-End'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Brafurries-Front-End');
+    expect(app.title).toEqual('BraFurries-Front-End');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Brafurries-Front-End');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, BraFurries-Front-End');
   });
 });
