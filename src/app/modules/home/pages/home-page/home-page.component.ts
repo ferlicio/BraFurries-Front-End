597cee95d17614577261c940c137c9579cb507d1
Fernando FR <ferlicio@gmail.com>
2024-03-26 13:55:24 -0300

    alteracoes home em progresso

--- src/app/modules/home/pages/home-page/home-page.component.ts
+++ src/app/modules/home/pages/home-page/home-page.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit{
+
+  constructor() {}
+
+  linkRedes = {
+    facebook: 'https://www.facebook.com/groups/brafurries/',
+    discord: 'https://discord.gg/brafurries',
+    telegram: 'https://t.me/brafurros',
+  }
+
+
+  ngOnInit() {
+    console.log('Home Page');
+  }
 
 }
