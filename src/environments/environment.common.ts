1b687060f5c5da76bef88db035ea2872a1687203
Fernando FR <ferlicio@gmail.com>
2025-08-07 16:21:45 -0300

    add environments

---
+++ src/environments/environment.common.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    linkRedes: {
+        facebook: 'https://www.facebook.com/groups/brafurries/',
+        discord: 'https://discord.gg/brafurries',
+        telegram: 'https://t.me/brafurros',
+        instagram: 'https://www.instagram.com/brafurries/',
+    },
+    production: true,
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+import { environment as baseEnv } from './environment.common';
+
+export const environment = {
+    ...baseEnv,
+};
